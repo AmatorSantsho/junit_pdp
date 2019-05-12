f445be869198787ee5e7a1658f2f438b26992ddf
AmatorSantsho <ash002@tut.by>
2019-05-12 21:16:07 +0200

    Third commit

--- src/main/java/com/epam/automation/junit/tests/SuiteTestClass.java
+++ src/main/java/com/epam/automation/junit/tests/SuiteTestClass.java
@@ -4,6 +4,6 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses({IntegerUtilsTest.class, StringUtilsTest.class})
+@Suite.SuiteClasses({StringUtilsTest.class,IntegerUtilsTest.class})
 public class SuiteTestClass {
 }
